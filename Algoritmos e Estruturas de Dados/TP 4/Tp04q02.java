1dff952435b631c05a6ef7b568c0b82630418e3e
ibryans <bryans0701@gmail.com>
2020-05-18 15:41:28 -0300

    Questão 01 pronta, começando questão 02

---
+++ Algoritmos e Estruturas de Dados/TP 4/Tp04q02.java
@@ -0,0 +1,365 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class Tp04q02 {
+    
+    public static boolean isNotEnd(String entry) {
+        return !(entry.length() >= 3 && entry.charAt(0) == 'F' && entry.charAt(1) == 'I' && entry.charAt(2) == 'M');
+    }
+
+    public static void main(String[] args) {
+        String entry = "", search = "";
+        Personagem p;
+        ArvoreArvore arvore = new ArvoreArvore();
+
+        // Leitura dos dados pra inserir na árvore
+        do {
+            entry = MyIO.readLine();
+            p = new Personagem();
+
+            if (isNotEnd(entry)) {
+                try {
+                    p.ler(entry);
+                    arvore.inserir(p);
+                }
+                catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+
+        } while (isNotEnd(entry));
+
+
+        // Pesquisa por nome
+        do {
+            search = MyIO.readLine();
+
+            if (isNotEnd(search)) {
+                arvore.pesquisar(search);
+            }
+
+        } while (isNotEnd(search));
+
+    }
+}
+
+/** 
+ * Classe de árvore Binária
+ * @author Bryan Santos Oliveira
+ */
+class ArvoreArvore {
+
+    NoPrimeiro raiz;
+
+    ArvoreArvore() {
+        raiz = null;
+    }
+
+    /**
+     * Insere um personagem na 2ª árvore com a chave na 1ª árvore
+     * @param p
+     * @throws Exception
+     */
+    private NoPrimeiro inserir(Personagem p, NoPrimeiro no) throws Exception {
+        if (no == null)
+            no = new NoPrimeiro(p.getAltura() % 15);
+        else if (p.getNome().compareTo(no.elemento.getNome()) > 0)
+            no.dir = inserir(p, no.dir);
+        else if (p.getNome().compareTo(no.elemento.getNome()) < 0)
+            no.esq = inserir(p, no.esq);
+        else
+            throw new Exception("Erro! Personagem já existe na árvore");
+        return no;
+    }
+
+    public void inserir(Personagem p) {
+        try {
+            raiz = inserir(p, raiz);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    /**
+     * Método privado que pesquisa por um personagem na árvore
+     * @param nome
+     * @param no
+     * @return um Booleano indicando se tem ou não
+     */
+    private boolean pesquisar(String nome, NoPrimeiro no) {
+        boolean resp = false;
+        if (no != null) {
+            if (no.elemento.getNome().equals(nome))
+                resp = true;
+            else if (nome.compareTo(no.elemento.getNome()) > 0) {
+                MyIO.print("dir ");
+                resp = pesquisar(nome, no.dir);
+            }
+            else {
+                MyIO.print("esq ");
+                resp = pesquisar(nome, no.esq);
+            }
+        }
+        return resp;
+    }
+
+    /**
+     * Pesquisa por um personagem na árvore e printa SIM ou NAO
+     * @param nome
+     */
+    public void pesquisar(String nome) {
+        MyIO.print(nome + " ");
+
+        MyIO.print("raiz ");
+        if (pesquisar(nome, this.raiz))
+            MyIO.println("SIM");
+        else
+            MyIO.println ( " N"+(char)195+"O" );
+    }
+
+}
+
+/** 
+ * O nó da primeira árvore binária 
+ */
+class NoPrimeiro {
+    NoPrimeiro esq, dir;
+    NoSegundo arvore;
+    int chave;
+
+    NoPrimeiro() {
+        this(0);
+    }
+
+    NoPrimeiro(int c) {
+        this.esq = this.dir = null;
+        this.arvore = new NoSegundo();
+        this.chave = c;
+    }
+}
+
+/**
+ * O nó da segunda árvore binária
+ */
+class NoSegundo {
+    NoSegundo esq, dir;
+    Personagem elemento;
+
+    NoSegundo() {
+        this(new Personagem());
+    }
+
+    NoSegundo(Personagem p) {
+        elemento = p;
+        esq = dir = null;
+    }
+}
+
+/**
+ * Classe de um personagem de star wars, relacionado à API 'SWAPI'
+ */
+class Personagem {
+
+    /*********** Atributos ***********/
+
+    private String nome, corDoCabelo, corDaPele, corDosOlhos, anoNascimento, genero, homeworld;
+
+    private int altura;
+
+    private double peso;
+
+    /********* Construtores *********/
+
+    public Personagem() {
+        this("");
+    }
+
+    public Personagem(String nome) {
+        this(nome, 0, 0.0, "", "", "", "", "", "");
+    }
+
+    public Personagem(String nome, int altura, double peso, String cabelo, String pele, String olhos, String nascimento,
+            String genero, String homeworld) {
+        this.nome = nome;
+        this.altura = altura;
+        this.peso = peso;
+        this.corDoCabelo = cabelo;
+        this.corDaPele = pele;
+        this.corDosOlhos = olhos;
+        this.anoNascimento = nascimento;
+        this.genero = genero;
+        this.homeworld = homeworld;
+    }
+
+    /******** Getters and setters ********/
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAltura() {
+        return this.altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return this.peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public String getCorDoCabelo() {
+        return this.corDoCabelo;
+    }
+
+    public void setCorDoCabelo(String corDoCabelo) {
+        this.corDoCabelo = corDoCabelo;
+    }
+
+    public String getCorDaPele() {
+        return this.corDaPele;
+    }
+
+    public void setCorDaPele(String corDaPele) {
+        this.corDaPele = corDaPele;
+    }
+
+    public String getCorDosOlhos() {
+        return this.corDosOlhos;
+    }
+
+    public void setCorDosOlhos(String corDosOlhos) {
+        this.corDosOlhos = corDosOlhos;
+    }
+
+    public String getAnoNascimento() {
+        return this.anoNascimento;
+    }
+
+    public void setAnoNascimento(String anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+
+    public String getGenero() {
+        return this.genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getHomeworld() {
+        return this.homeworld;
+    }
+
+    public void setHomeworld(String homeworld) {
+        this.homeworld = homeworld;
+    }
+
+    /**** Métodos da classe *****/
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public String pegarAtributo(String json, String atributo) throws Exception {
+
+        boolean achado = false;
+        String valor = "";
+        int i = 0;
+
+        // Vai até o atributo escolhido
+        while (!achado && i < json.length()) {
+
+            // Verifica se é aspas simples (39 = ')
+            if (json.charAt(i) == 39) {
+
+                i++;
+                String nome = "";
+
+                // Percorrer o conteudo dentro das aspas simples e salva em nome
+                while (i < json.length() && json.charAt(i) != 39) {
+                    nome += json.charAt(i);
+                    i++;
+                } 
+
+                // Se o nome achado for o atributo escolhido
+                if (nome.equals(atributo)) {
+                    achado = true;
+                }
+
+            } else {
+                i++;
+            }
+        }
+
+        // Se saiu do while e não achou o atributo
+        if (!achado) {throw new Exception("Atributo não encontrado!");}
+
+        // Vai pro começo do valor do atributo
+        i += 4;
+
+        while (i < json.length() && json.charAt(i) != 39) {
+            valor += json.charAt(i);
+            i++;
+        }
+
+        return valor;
+    }
+
+    public void ler(String path) throws Exception {
+        File file = new File(path);
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String json = reader.readLine();
+
+        // Salvando os atributos
+
+        this.nome = this.pegarAtributo(json, "name");
+
+        String altura = this.pegarAtributo(json, "height");
+        this.altura = (altura.equals("unknown")) ? 0 : Integer.parseInt(altura);
+
+        String peso = this.pegarAtributo(json, "mass");
+        if (peso.equals("unknown")) {
+            this.peso = 0;
+        }
+        else { 
+            this.peso = (peso.split(",").length > 1)
+            ? Double.parseDouble(peso.split(",")[0] + peso.split(",")[1])
+            : Double.parseDouble(peso.split(",")[0]); 
+        }
+
+        this.corDoCabelo = this.pegarAtributo(json, "hair_color");
+        this.corDaPele = this.pegarAtributo(json, "skin_color");
+        this.corDosOlhos = this.pegarAtributo(json, "eye_color");
+        this.anoNascimento = this.pegarAtributo(json, "birth_year");
+        this.genero = this.pegarAtributo(json, "gender");
+        this.homeworld = this.pegarAtributo(json, "homeworld");
+
+        reader.close();
+    }
+
+    public void imprimir() {
+        MyIO.print(" ## " + this.nome);
+        MyIO.print(" ## " + this.altura);
+        MyIO.print(" ## " + String.valueOf(this.peso).replace(".0", ""));
+        MyIO.print(" ## " + this.corDoCabelo);
+        MyIO.print(" ## " + this.corDaPele);
+        MyIO.print(" ## " + this.corDosOlhos);
+        MyIO.print(" ## " + this.anoNascimento);
+        MyIO.print(" ## " + this.genero);
+        MyIO.println(" ## " + this.homeworld + " ##");
+    }
+
+}
